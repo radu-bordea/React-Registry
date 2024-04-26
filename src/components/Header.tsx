8a88aa693ae9d0d1e05b32ba3ee5c125b2403d31
radu-bordea <radu.bordea.dev@gmail.com>
2024-04-26 10:14:25 +0200

    create search input component and implement navigate by search term

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom"
+import SearchInput from "./SearchInput"
+
 export default function Header() {
-    return <div>Header</div>
+    return <div>
+        <Link to="/">NPM Registry</Link>
+        <SearchInput/>
+    </div>
 }
